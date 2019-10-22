8534c228ac25d21bca585a1a17743cd902be98a9
Anna Bettina Ovsiyovna Steinberg <steinberga@cip.ifi.lmu.de>
2019-10-22 12:07:07 +0200

    first test added

--- threads/models.py
+++ threads/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Entity(models.Model):
+    entity_id = models.CharField(max_length=200)
+    platform_id = models.CharField(max_length=200)
+
+
 class Thread(models.Model):
     title = models.CharField(max_length=200)
+    entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
 
 
 class Author(models.Model):
@@ -12,7 +18,7 @@ class Author(models.Model):
     platform_id = models.CharField(max_length=200)
 
 
-class Commment(models.Model):
+class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
--- threads/tests.py
+++ threads/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from threads.models import *
+from django.urls import reverse
 
 # Create your tests here.
+
+
+def create_thread(**kwargs):
+    author = Author.objects.create(
+        user_id=kwargs['user_id'], platform_id=kwargs['platform_id'])
+    entity = Entity.objects.create(
+        entity_id=kwargs['entity_id'], platform_id=kwargs['platform_id'])
+    thread = Thread.objects.create(title=kwargs['title'], entity=entity)
+    comment = Comment.objects.create(
+        author=author, content=kwargs['content'], thread=thread)
+    return thread
+
+
+class ThreadIndexViewTests(TestCase):
+    def test_no_thread(self):
+        url = reverse('threads:index', args=('serlo', '123'))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "[]")
